42113342fb5c80f5ba6bc964978dd927d85a1e76
Guilherme Leite <guilhermelax@hotmail.com>
2024-01-03 16:05:19 -0300

    Feat: Creating the Contacts Controller and a GetAll Method.

---
+++ ContactsAPI/Controllers/ContactsController.cs
@@ -0,0 +1,23 @@
+﻿using ContactsAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContactsController : Controller
+    {
+        private readonly ContactsAPIDbContext _dbContext;
+
+        public ContactsController(ContactsAPIDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_dbContext.Contacts.ToList());
+        }
+    }
+}
